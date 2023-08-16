ad3cd859ea72dd9b559db8271dcf4f6a0f9b0186
venkatesh ponraj <vengatp14@gmail.com>
2023-08-16 14:39:36 +0530

    init

---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+// Initialize AOS 
+AOS.init();
+
+// dark mode light mode 
+const toggle = document.querySelector('dark-mode-toggle');
+const body = document.body;
+
+// Set or remove the `dark` class the first time.
+toggle.mode === 'dark' ? body.classList.add('dark') : body.classList.remove('dark');
+
+// Listen for toggle changes (which includes `prefers-color-scheme` changes)
+// and toggle the `dark` class accordingly.
+toggle.addEventListener('colorschemechange', () => {
+  body.classList.toggle('dark', toggle.mode === 'dark');
+});
